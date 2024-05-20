a66b4c8428bb4770c7a77feb3ec0e8eb9cc4afd1
Guilherme Inacio <gui-inacio-2010@hotmail.com>
2024-05-20 18:15:00 -0300

    feat: add code style configs

--- src/server/server.ts
+++ src/server/server.ts
@@ -2,8 +2,12 @@ import express from 'express';
 
 const server = express();
 
+interface Teste {
+
+}
+
 server.get('/', (req, res) => {
-    return res.send('Ola dev!')
+  return res.send('Ola dev!')
 });
 
 export { server };
