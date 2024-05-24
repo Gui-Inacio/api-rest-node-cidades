be6180dab3b32ca244dbf57f0e985c34a72c40c4
Guilherme Inacio <gui-inacio-2010@hotmail.com>
2024-05-24 20:57:56 -0300

    errors message created

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -5,23 +5,33 @@ import * as yup from 'yup';
 
 interface ICidade {
   nome: string;
+  //estado: string;
 }
 
 const bodyValidation: yup.ObjectSchema<ICidade> = yup.object().shape({
   nome: yup.string().required().min(3),
+  //estado: yup.string().required().min(3),
 })
 
 export const create = async (req: Request<{}, {}, ICidade>, res: Response) => {
   let validatedData: ICidade | undefined = undefined;
 
   try {
-    validatedData = await bodyValidation.validate(req.body);
+    validatedData = await bodyValidation.validate(req.body, { abortEarly: false });
   } catch (error) {
     const yupError = error as yup.ValidationError;
-    return res.json({
-      error: {
-        default: yupError.message,
-      }
+    const validationErrors: Record<string, string> = {};
+
+    yupError.inner.forEach(error => {
+      error.message
+      if (error.path === undefined) return;
+
+      validationErrors[error.path] = error.message;
+    });
+
+
+    return res.status(StatusCodes.BAD_REQUEST).json({
+      errors: validationErrors,
     });
   }
 
